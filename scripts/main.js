352ddc95938118b1c3fa1de7715b492fb602c3d4
MicheleOM <115908319+MicheleOM@users.noreply.github.com>
2026-01-20 01:34:47 -0300

    estrutura inicial portifólio

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  const navItems = document.querySelectorAll(".nav-links a");
+
+  toggleButton.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+
+  navItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      navLinks.classList.remove("active");
+    });
+  });
+});
